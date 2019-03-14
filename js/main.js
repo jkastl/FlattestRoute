fd37174c115c8c9d2d516fa9f85889fbda07b07c
Jeff Kastl <jkastl@rednovalabs.com>
2019-03-14 15:56:31 -0500

    update default loc

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ function initialize_maps() {
     directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
     var mapCanvas = $('#map-canvas').get(0);
     var mapOptions = {
-        center: new google.maps.LatLng(37.787930,-122.4074990),
+        center: new google.maps.LatLng(43.6741532,-93.0087578),
         zoom: 13,
         // Disables zoom and streetview bar but can stil zoom with mouse.
         disableDefaultUI: false,
